abaece34ceff5fb8bbefeb4f3190612f34840b1e
Jhonnyg <jhonny@student.chalmers.se>
2010-11-22 17:50:35 +0100

    push

--- dbass/task2/db.sql
+++ dbass/task2/db.sql
@@ -18,9 +18,17 @@ INSERT INTO Course
 	(code,name,credits,depname)
 	VALUES ('TDA357','Databases',10, 'Computing Science');
 	
+INSERT INTO Course
+	(code,name,credits,depname)
+	VALUES ('TDA335','Programming Project, Major',10,'Computing Science');
+
+INSERT INTO BranchMandatory
+	VALUES ('Computer Science and Engineering','Computer Languages','TDA335');
 	
-	
-	
+INSERT INTO BranchMandatory
+	VALUES ('Computer Science and Engineering','Computer Languages','TDA357');
+
+-- Student
 INSERT INTO Student
 	VALUES (8406118235, 'Jhonny Göransson', 'Computer Languages', 'Computer Science and Engineering');
 
@@ -30,3 +38,8 @@ INSERT INTO Student
 INSERT INTO Student
 	VALUES (8611283550,'Ingemar Åhdal', 'Computer Languages', 'Computer Science and Engineering');
 	
+INSERT INTO Registered 
+	VALUES (8601011337,'TDA357');
+
+INSERT INTO HasTaken
+	VALUES (8406118235,'TDA357',5);
--- dbass/task2/views.sql
+++ dbass/task2/views.sql
@@ -1,4 +1,22 @@
 CREATE VIEW DBStudents AS
-	SELECT name,programme,branch 
+	SELECT Student.name as name,programme,branch 
 	FROM Student,Branch
-	WHERE programme = Branch.progname
+	WHERE programme = Branch.progname;
+
+CREATE VIEW DBFinishedCourses AS 
+	SELECT Student.name as name,Course.name as coursename, grade
+	FROM HasTaken,Student,Course
+	WHERE Student.persnumber = HasTaken.persnumber
+	AND Course.code = HasTaken.code;
+	
+
+
+--For all students, the mandatory courses (branch and programme) they have not yet taken.
+/*
+SELECT Student.name as name,Course.name as coursename
+FROM Student,BranchMandatory,ProgrammeMandatory,Course
+WHERE ProgrammeMandatory.code = Course.code OR BranchMandatory.code = Course.code
+
+SELECT Student.name as name,Course.name as coursename
+FROM Student,HasTaken,Course
+WHERE Student.persnumber = HasTaken.persnumber AND Course.code = HasTaken.code; */
