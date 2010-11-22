e72c8806d66fb5205d6ed9bdb95f95f5996ba4ea
sweetfish <sweetfish@gmail.com>
2010-11-22 17:34:15 +0100

    aoe

--- dbass/task2/views.sql
+++ dbass/task2/views.sql
@@ -2,3 +2,13 @@ CREATE VIEW DBStudents AS
 	SELECT name,programme,branch 
 	FROM Student,Branch
 	WHERE programme = Branch.progname
+
+CREATE VIEW DBStudentStatus AS
+	SELECT * FROM (SELECT Student.name as studentname,Course.name as coursename,'registered' as status
+		FROM Student,Registered,Course
+		WHERE Student.persnumber = Registered.persnumber
+			AND Course.code = Registered.code) UNION
+	(SELECT Student.name as studentname,Course.name as coursename,'waiting' as status
+		FROM Student,WaitingList,Course
+		WHERE Student.persnumber = WaitingList.persnumber
+			AND Course.code = WaitingList.code);
