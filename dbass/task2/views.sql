7b42720e6188c71fe1f850fbec20f4aac3883d8e
sweetfish <sweetfish@gmail.com>
2010-11-23 15:47:53 +0100

    small progress

--- dbass/task2/views.sql
+++ dbass/task2/views.sql
@@ -1,23 +1,23 @@
 -- For all students, their names, and the programme and branch they are following.
 CREATE VIEW DBStudents AS
-	SELECT Student.name as name,programme,branch 
+	SELECT Student.persnumber as persnumber, Student.name as name,programme,branch 
 	FROM Student,Branch
 	WHERE programme = Branch.progname
 
 -- For all students, all finished courses, along with their grades.
 CREATE VIEW DBFinishedCourses AS 
-	SELECT Student.name as name,Course.name as coursename, grade
+	SELECT Student.persnumber as persnumber, Student.name as name, Course.name as coursename, grade
 	FROM HasTaken,Student,Course
 	WHERE Student.persnumber = HasTaken.persnumber
 	AND Course.code = HasTaken.code;
 
 -- All registered and waiting students for all courses, along with their waiting status ('registered' or 'waiting').
 CREATE VIEW DBStudentStatus AS
-	SELECT * FROM (SELECT Student.name as studentname,Course.name as coursename,'registered' as status
+	SELECT * FROM (SELECT Student.persnumber as persnumber, Student.name as name,Course.name as coursename,'registered' as status
 		FROM Student,Registered,Course
 		WHERE Student.persnumber = Registered.persnumber
 			AND Course.code = Registered.code) UNION
-	(SELECT Student.name as studentname,Course.name as coursename,'waiting' as status
+	(SELECT Student.persnumber as persnumber, Student.name as name, Course.name as coursename,'waiting' as status
 		FROM Student,WaitingList,Course
 		WHERE Student.persnumber = WaitingList.persnumber
 			AND Course.code = WaitingList.code);
@@ -25,25 +25,25 @@ CREATE VIEW DBStudentStatus AS
 
 -- For all students, the mandatory courses (branch and programme) they have not yet taken.
 CREATE VIEW DBMandatoryCourses AS 
-	((SELECT Student.name as name, Course.name as coursename
+	((SELECT Student.persnumber as persnumber, Student.name as name, Course.name as coursename
 		FROM Student,BranchMandatory,Course
 		WHERE BranchMandatory.branch = Student.branch AND BranchMandatory.programme = Student.programme AND Course.code = BranchMandatory.code)
 		UNION
-		(SELECT Student.name as name, Course.name as coursename
+		(SELECT Student.persnumber as persnumber, Student.name as name, Course.name as coursename
 			FROM Student,ProgrammeMandatory,Course
 			WHERE ProgrammeMandatory.programme = Student.programme AND Course.code = ProgrammeMandatory.code))
 	MINUS
-	(SELECT Student.name as name,Course.name as coursename
+	(SELECT Student.persnumber as persnumber, Student.name as name,Course.name as coursename
 		FROM Student,HasTaken,Course
 		WHERE Student.persnumber = HasTaken.persnumber AND Course.code = HasTaken.code);
 
 -- For all students, the recommended courses of their branch that they have not yet taken.
 CREATE VIEW DBRecommendedCourses AS 
-	(SELECT Student.name as name, Course.name as coursename
+	(SELECT Student.persnumber as persnumber, Student.name as name, Course.name as coursename
 		FROM Student,BranchRecommended,Course
 		WHERE BranchRecommended.branch = Student.branch AND BranchRecommended.programme = Student.programme AND Course.code = BranchRecommended.code)
 	MINUS
-	(SELECT Student.name as name,Course.name as coursename
+	(SELECT Student.persnumber as persnumber, Student.name as name, Course.name as coursename
 		FROM Student,HasTaken,Course
 		WHERE Student.persnumber = HasTaken.persnumber AND Course.code = HasTaken.code);
 
@@ -52,7 +52,14 @@ CREATE VIEW DBRecommendedCourses AS
 SELECT Student.persnumber, SUM(Course.credits)
 	FROM Student LEFT OUTER JOIN HasTaken ON Student.persnumber = HasTaken.persnumber LEFT OUTER JOIN Course ON HasTaken.code = Course.code
 	GROUP BY Student.persnumber);
-	
+
+
+-- the number of mandatory courses they have yet to read (branch or programme).
+SELECT Student.persnumber, SUM(Course.credits)
+	FROM Student LEFT OUTER JOIN DBMandatoryCourses ON Student.persnumber = DBMandatoryCourses.persnumber LEFT OUTER JOIN Course ON HasTaken.code = Course.code
+	GROUP BY Student.persnumber);
+
+
 /*
 (SELECT Student.persnumber, SUM(Course.credits) as credits
 FROM Student JOIN HasTaken ON Student.persnumber =  HasTaken.persnumber
